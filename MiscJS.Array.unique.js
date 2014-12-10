670e682e5eef2c8cff3fc0a948b4e08687fc95bc
Steve Bink <routinet@users.noreply.github.com>
2014-12-10 14:41:30 -0500

    Update MiscJS.Array.unique.js

--- MiscJS.Array.unique.js
+++ MiscJS.Array.unique.js
@@ -1,18 +1,20 @@
-/* Adds .unique() method to arrays
-   Returns an array with all duplicates removed
-*/
-Object.defineProperty(Array.prototype,'unique',
-    {
-      writable: true,
-      value: function(){
-        var u = {}, a = [];
-        for(var i = 0, l = this.length; i < l; ++i){
-          if(u.hasOwnProperty(this[i])) {
-             continue;
+/* Add Array.unique() function to javascript arrays
+   Returns a new array including only unique values found in the original
+   */
+if (!Array.prototype.unique) {
+  Object.defineProperty(Array.prototype,'unique',
+      {
+        writable: true,
+        value: function(){
+          var u = {}, a = [];
+          for(var i = 0, l = this.length; i < l; ++i){
+            if(u.hasOwnProperty(this[i])) {
+               continue;
+            }
+            a.push(this[i]);
+            u[this[i]] = 1;
           }
-          a.push(this[i]);
-          u[this[i]] = 1;
+          return a;
         }
-        return a;
-      }
-    });
+      });
+}
